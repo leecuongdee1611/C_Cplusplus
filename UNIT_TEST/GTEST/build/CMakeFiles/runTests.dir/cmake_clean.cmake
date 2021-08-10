cb043e4b9c437938643093e2161de714f0b511d8
cuonglehung1611 <leecuong.dee1611@gmail.com>
2021-08-10 09:47:55 +0700

    Unit Test

---
+++ UNIT_TEST/GTEST/build/CMakeFiles/runTests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTests.dir/function_need_to_test.cpp.o"
+  "CMakeFiles/runTests.dir/test.cpp.o"
+  "runTests"
+  "runTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
